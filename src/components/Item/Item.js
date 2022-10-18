29ccf7c9119819811a003bc03acdab226c074661
zinukk <slsxpseh54@naver.com>
2022-10-18 15:21:10 +0900

    ADD : 공용 컴포넌트 item.js 파일 추가

---
+++ src/components/Item/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const item = props => {
+  return <div></div>;
+};
+
+export default item;
--- src/components/Nav/NavMiddleBox.js
+++ src/components/Nav/NavMiddleBox.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import { faCaretSquareRight } from '@fortawesome/free-regular-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import {
   faCartShopping,
