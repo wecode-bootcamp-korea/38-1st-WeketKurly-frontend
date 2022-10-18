005431ad2d3b6606d89f468db21a1acd39f87c8f
zinukk <slsxpseh54@naver.com>
2022-10-18 16:14:05 +0900

    ADD : items 공용컴포넌트 추가

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './Item.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const item = props => {
-  return <div></div>;
+  const { imgUrl, item, price } = props;
+  return (
+    <div className="itemContainer">
+      {/* src 부분에 이미지 변수로 넣으면 됩니다 */}
+      <img
+        className="itemImg"
+        src="https://img-cf.kurly.com/cdn-cgi/image/width=400,format=auto/shop/data/goods/1650258085245l0.jpg"
+        alt="상품 이미지"
+      />
+      <div className="itemIconBox">
+        <FontAwesomeIcon className="itemIcon" icon={faCartShopping} />
+      </div>
+      {/* 텍스트 부분에 description 변수로 넣으면 됩니다 */}
+      <p className="itemDescription">[더오담] 포켓 쏙 현미 누룽지 (국내산)</p>
+      {/* 텍스트 부분에 price 변수로 넣으면 됩니다 */}
+      <p className="itemPrice">7,980원</p>
+    </div>
+  );
 };
 
 export default item;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
+import Item from '../../components/Item/Item';
 import './Main.scss';
 
 const Main = () => {
   return (
     <div>
       <Nav />
+      <Item />
     </div>
   );
 };
